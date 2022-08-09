160d9cb5ee2d299ae6aba478adaf5af3875de229
bessyhuang <bessyhuang1@gmail.com>
2022-08-09 16:32:40 +0800

    FastAPI connect to MongoDB

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # fastapi-mongodb-tutorial
+
+## MongoDB Python Drivers
+1. Use `PyMongo` for synchronous Python applications.
+2. Use `Motor` for asynchronous Python applications.
+* Reference:
+  * [MongoDB Documentation](https://www.mongodb.com/docs/drivers/python/)
+  * [PyMongo vs Motor](https://gist.github.com/anand2312/840aeb3e98c3d7dbb3db8b757c1a7ace)
---
+++ main.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI
+from typing import Union
+import uvicorn
+import motor.motor_asyncio
+
+MONGODB_INFO = "mongodb://127.0.0.1:27017"
+DATABASE = "FabryDisease"
+COLLECTION = "Clinical"
+
+# 1. Connect to MongoDB by using motor
+def get_server_info(MONGODB_INFO):
+    # replace this with your MongoDB connection string
+    conn_str = MONGODB_INFO
+    # set a 5-second connection timeout
+    client = motor.motor_asyncio.AsyncIOMotorClient(conn_str, serverSelectionTimeoutMS=5000)
+    try:
+        print(client.server_info())
+    except Exception:
+        print("Unable to connect to the server.")
+    return client
+
+# 2. Select specific fields and display on FastAPI interface 
+def coll_helper(coll_clinical) -> dict:
+    # return specific fields as key:value pair
+    return { 
+        "id": str(coll_clinical["_id"]), 
+        "SampleID": coll_clinical["SampleID"],
+        "Group": coll_clinical["Group"],
+        "Age": coll_clinical["Age"],
+        "date of birth": coll_clinical["date of birth"],
+        "LVMI": coll_clinical["LVMI"],
+        "ERT drugs": coll_clinical["ERT drugs"]
+    }
+
+def ResponseModel(data, message):
+    return {
+        "data": [data],
+        "code": 200,
+        "message": message,
+    }
+
+# 3. Retrieve all documents in collection present in the database
+async def retrieve_coll(COLLECTION):
+    new_coll = []
+    collection = db.get_collection(COLLECTION)
+    async for coll in collection.find():
+        new_coll.append(coll_helper(coll))
+    return new_coll
+
+
+client = get_server_info(MONGODB_INFO)
+db = client[DATABASE]
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/GET_coll", response_description="Clinical data retrieved")
+async def get_coll():
+    clincals = await retrieve_coll(COLLECTION)
+    if clincals:
+        return ResponseModel(clincals, "Data retrieved successfully")
+    else:
+        return ResponseModel(clincals, "Empty list returned")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="10.64.16.166", port=8000)
