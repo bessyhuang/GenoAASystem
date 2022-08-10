416276a43f20022b4cd4d83f2c0311376128015b
bessyhuang <bessyhuang1@gmail.com>
2022-08-10 16:39:38 +0800

    Fix

--- EXAMPLE_Use_plink_on_R940B.sh
+++ EXAMPLE_Use_plink_on_R940B.sh
@@ -2,15 +2,14 @@
 
 takeda_home='/home/takeda_user1/'
 
-filename='FabryDisease_93.list'
-Input_VCF_FilePath='/NovaSeq_127/FabryDisease/WGS/Parabricks/hg38/VCF/'
+Input_VCF_FilePath='/NovaSeq_128/takeda_output/'
+Input_VCF_FileName='Fabry_93.merge.vcf'
 
 disease='Fabry'
 Amount_of_Samples='93'
 
-Output_FilePath='/NovaSeq_128/takeda_output/'
-[ ! -d "$Output_FilePath" ] && mkdir -p "$Output_FilePath"
+Output_FilePath='/NovaSeq_128/takeda_output/fam_file/'
+Output_File_prefix="new_${disease}_${Amount_of_Samples}_bfile"
 
-sshpass -p 'takeda_user1' ssh takeda_user1@10.64.16.166 "ls ${Input_VCF_FilePath} > ${takeda_home}FabryDisease_93_new.list"
-sshpass -p 'takeda_user1' ssh takeda_user1@10.64.16.166 "sed -e 's/^DPFWGS005/\/NovaSeq_127\/FabryDisease\/WGS\/Parabricks\/hg38\/VCF\/DPFWGS005/' -i ${takeda_home}FabryDisease_93_new.list"
-sshpass -p 'takeda_user1' ssh takeda_user1@10.64.16.166 "/bin/bcftools_v1.15.1/bin/bcftools merge -0 -l ${takeda_home}FabryDisease_93_new.list --threads 16 --force-samples --no-index -Ov -o ${Output_FilePath}${disease}_${Amount_of_Samples}.merge.vcf"
+sshpass -p 'takeda_user1' ssh takeda_user1@10.64.16.166 "[ ! -d $Output_FilePath ] && mkdir -p $Output_FilePath"
+sshpass -p 'takeda_user1' ssh takeda_user1@10.64.16.166 "plink --vcf ${Input_VCF_FilePath}${Input_VCF_FileName} --make-bed --out ${Output_FilePath}${Output_File_prefix}"
