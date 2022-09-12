a407a2a41e5f2f455541f4ece74b887929debb5e
bessyhuang <bessyhuang1@gmail.com>
2022-09-12 15:07:17 +0800

    Build Filter Form

--- app/src/endpoints/FilterClinicals.py
+++ app/src/endpoints/FilterClinicals.py
@@ -7,7 +7,8 @@ from fastapi import APIRouter
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
 
-
+from typing import Union
+from fastapi import Form
 
 templates = Jinja2Templates(directory="app/templates")
 
@@ -36,4 +37,44 @@ async def FilterClinicals(request: Request):
     pipeline = [{"$match": {'Group': '非ERT組'}}]
 
     LIST_clins = await cursor.aggregate(pipeline).to_list(length=None)
+    return templates.TemplateResponse("FilterClins.html", context={"request": request, "clins": LIST_clins})
+
+
+@router.get("/")
+async def parameter_form(request: Request):
+    return templates.TemplateResponse('InputForm.html', context={'request': request})
+
+@router.post("/")
+async def parameter_form(
+    *, request: Request, 
+    age: int = Form(...), LVMI: float = Form(...), 
+    group: str = Form(...), ERT_drug: str = Form(...),
+    response_model=schemas.ClinicalModel):
+    
+    results = {"age": age, "LVMI": LVMI, "group": group, "ERT_drug": ERT_drug}
+    #return templates.TemplateResponse('FilteredFormResult.html', context={'request': request, 'result': results})
+
+    COLLECTION = "Clinical"
+    cursor = db[COLLECTION]
+    DISPLAYED_FIELD = { "_id": 0, "SampleID": 1, "Group": 1, "Age": 1, "LVMI": 1, "microalbumin": 1, "Gender": 1, "ERT drugs": 1, "IVSD before ERT": 1, "Heart MRI LGE (fibrosis)": 1, "Remark": 1 }
+
+    if results['ERT_drug'] == 'No selected':
+        pipeline = [
+            {'$match': {'Group': results['group']}},
+            {'$match': {'Age':  { '$gte': results['age']}}},
+            {'$match': {'LVMI': { '$gte': results['LVMI']}}},
+            {'$project': DISPLAYED_FIELD}
+        ]
+    else:
+        pipeline = [
+            {'$match': {'Group': results['group']}},
+            {'$match': {'Age':  { '$gte': results['age']}}},
+            {'$match': {'LVMI': { '$gte': results['LVMI']}}},
+            {'$match': {'ERT drugs': results['ERT_drug']}},
+            {'$project': DISPLAYED_FIELD}
+        ]
+
+    #print(pipeline)
+    LIST_clins = await cursor.aggregate(pipeline).to_list(length=None)
+    print(LIST_clins)
     return templates.TemplateResponse("FilterClins.html", context={"request": request, "clins": LIST_clins})
